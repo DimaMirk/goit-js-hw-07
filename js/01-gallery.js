ce56dcdc0a1c038b725bb8b083506da145ac0ef9
Dmytro <dmytro@paybee.io>
2023-01-21 22:51:58 +0200

    fixed few issue

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,35 +7,36 @@ const gallery = document.querySelector('.gallery');
 const createLi = galleryItems
   .map(
     (elem) =>
-      `<a class="gallery__link"><img src = '${elem.preview}' class="gallery__image" alt='${elem.description}'  data-secondSrc=${elem.original}'></a>`
+      `<a class="gallery__link"><img src = '${elem.preview}' class="gallery__image" alt='${elem.description}'  data-secondSrc='${elem.original}'></a>`
   )
   .join('');
 
-// gallery.insertAdjacentHTML('beforeEnd', createLi);
 gallery.innerHTML = createLi;
 
 const showBigPicture = (evn) => {
-  const imgBigSizeValue = evn.target.dataset.secondsrc;
+  if(evn.target.classList.contains('gallery__image')){
+    const imgBigSizeValue = evn.target.dataset.secondsrc;
 
-  const instance = basicLightbox
-    .create(
-      `
-  	<img width="1400" height="900" src="${imgBigSizeValue}">
-    `,
-      {
-        onShow: (instance) => {
-          const checkFunk = (event) => {
-            if (event.code == 'Escape') {
-              instance.close(
-                document.removeEventListener('keydown', checkFunk)
-              );
-            }
-          };
-          document.addEventListener('keydown', checkFunk);
-        },
-      }
-    )
-    .show();
+    const instance = basicLightbox
+      .create(
+        `
+      <img width="1400" height="900" src="${imgBigSizeValue}">
+      `,
+        {
+          onShow: (instance) => {
+            const checkFunk = (event) => {
+              if (event.code == 'Escape') {
+                instance.close(
+                  document.removeEventListener('keydown', checkFunk)
+                );
+              }
+            };
+            document.addEventListener('keydown', checkFunk);
+          },
+        }
+      )
+      .show();
+  }
 };
 
 gallery.addEventListener('click', showBigPicture);
